8a40872977290340bb3854499b645bdb816ffb1a
Raj <rajexcited@users.noreply.github.com>
2024-08-16 07:36:04 -0500

    renamed index files as unable to prevent or ignore commit of index file

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,5 +1,14 @@
-const demoMock = {
-  history: { isDummy: true },
-};
+import demoMock from "./mock";
+import { MockLogin } from "./services/mock-login";
+import { MockConfigType } from "./services/mock-config-type";
+import { MockPaymentAccounts } from "./services/mock-pymt-accounts";
+import { MockExpenses } from "./services/mock-expenses";
+import { MockPurchase } from "./services/mock-purchase";
+
+MockLogin(demoMock);
+MockConfigType(demoMock);
+MockPaymentAccounts(demoMock);
+MockExpenses(demoMock);
+MockPurchase(demoMock);
 
 export default demoMock;
